aa4b3d27892630e4bf7475a810ea4488393449d1
Codegeek2004 <singhjanvi2021@gmail.com>
2024-12-16 14:12:05 +0530

    10th commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/header';
 
-
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <div className="app">fef</div>
+    </>
   );
 }
 
