bb3ff78bbecb7e6acb3f922473ceb79f2595ec58
Codegeek2004 <singhjanvi2021@gmail.com>
2024-12-19 10:05:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/header';
+import SimpleBottomNavigation from './components/mainnav';
 
 function App() {
   return (
     <>
       <Header />
       <div className="app">fef</div>
+      <SimpleBottomNavigation />
     </>
   );
 }
