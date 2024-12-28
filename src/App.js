cac288effc6ef4891c61fa06f37b388159831bf7
Codegeek2004 <singhjanvi2021@gmail.com>
2024-12-28 19:23:46 +0530

    creating trending page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header/header';
 import SimpleBottomNavigation from './components/mainnav';
 import { BrowserRouter } from 'react-router-dom';
 import Container from '@mui/material/Container'; // For Material-UI
+import { Favorite } from '@mui/icons-material';
 
 
 function App() {
@@ -11,7 +12,15 @@ function App() {
     <BrowserRouter>
       <Header />
       <div className = "app">
-      <Container>Movie App</Container>
+      <Container>
+        <Switch>
+          <Route path ='/' component={Trending}exact/>
+          <Route path ='/search' component={Search}/>
+          <Route path ='/movies' component={Movies}/>
+          <Route path ='/series' component={Series}/>
+          <Route path ='/favourites' component={Favorites}/>
+        </Switch>
+      </Container>
       </div>
       <div style={{ marginBottom: '60px' }}> {/* To avoid overlap */}</div>
       <SimpleBottomNavigation />
